0c7c7e34231adf6e281b98378caa5aea33c87879
Padmaja <pvemparala@hotmail.com>
2020-08-24 15:48:35 -0700

    Add for and if-else loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 var pokemonList = [
-  { name: "Bulbasaur", height: 7, type: ["grass", "poison"] },
+  { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
   { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
   { name: "Butterfree", height: 1.1, type: ["bug", "flying"] },
 ];
+for (var i = 0; i < pokemonList.length; i++) {
+  var pokemonHeight = pokemonList[i].height;
+  var pokemonName = pokemonList[i].name;
+  if (pokemonHeight > 1.5) {
+    document.write(
+      "<h3>" +
+        pokemonName +
+        "(Height: " +
+        pokemonHeight +
+        ")   " +
+        " - Wow this is big<br></h3>"
+    );
+  } else {
+    document.write(
+      "<h3>" + pokemonName + "(Height: " + pokemonHeight + ")<br>   </h3> "
+    );
+  }
+}
