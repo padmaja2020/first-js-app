b796785f1acc701b11824201b79de66d15681e6d
Padmaja <pvemparala@hotmail.com>
2020-08-24 18:28:33 -0700

    Add forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,15 @@ var pokemonList = [
   { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
   { name: "Butterfree", height: 1.1, type: ["bug", "flying"] },
 ];
+
+document.write("<h1> forEach Loop</h1><br>");
+pokemonList.forEach(function (list) {
+  document.write(
+    "<p>" + list.name + "  " + list.height + "   " + list.type + "<br>   </p> "
+  );
+});
+
+document.write("<br><h1> for Loop</h1><br>");
 for (var i = 0; i < pokemonList.length; i++) {
   var pokemonHeight = pokemonList[i].height;
   var pokemonName = pokemonList[i].name;
