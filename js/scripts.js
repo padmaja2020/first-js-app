e914df06833b367b81e6d220da89efcecdc07ea7
Padmaja <pvemparala@hotmail.com>
2020-08-25 19:01:06 -0700

    Use IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,42 @@
-var pokemonList = [
-  { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
-  { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
-  { name: "Butterfree", height: 1.1, type: ["bug", "flying"] },
-];
+var pokemonRepository = (function () {
+  var pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+    { name: "Charizard", height: 1.7, type: ["fire", "flying"] },
+    { name: "Butterfree", height: 1.1, type: ["bug", "flying"] },
+  ];
 
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    return pokemonList.push(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+pokemonRepository.add({
+  name: "pikachoo",
+  height: "2",
+  type: ["fire", "air"],
+});
+var newRepository = pokemonRepository.getAll();
 document.write("<h1> forEach Loop</h1><br>");
-pokemonList.forEach(function (list) {
+newRepository.forEach(function (list) {
   document.write(
     "<p>" + list.name + "  " + list.height + "   " + list.type + "<br>   </p> "
   );
 });
 
 document.write("<br><h1> for Loop</h1><br>");
-for (var i = 0; i < pokemonList.length; i++) {
-  var pokemonHeight = pokemonList[i].height;
-  var pokemonName = pokemonList[i].name;
-  if (pokemonHeight > 1.5) {
+for (var i = 0; i < newRepository.length; i++) {
+  var pokemonHeight = newRepository[i].height;
+  var pokemonName = newRepository[i].name;
+  if (pokemonHeight > 1.9) {
     document.write(
       "<h3>" +
         pokemonName +
